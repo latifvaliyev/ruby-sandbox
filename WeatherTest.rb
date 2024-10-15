32bf2c824e87b466c0e7660354ebcaae223aafaa
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-10-15 22:38:22 +0000

    weather test

---
+++ WeatherTest.rb
@@ -0,0 +1,14 @@
+require "http"
+
+# Hidden variables
+Google_maps_api_key = ENV["AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"]
+
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+
+Google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=#{Google_maps_api_key}"
+
+raw = HTTP.get(Google_maps_url)
+
+require "json"
+
+
