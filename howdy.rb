1c453218a61fe2734bf4f138f03b825e99a62500
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-10-15 22:16:13 +0000

    start of ruby stuff

---
+++ howdy.rb
@@ -0,0 +1,16 @@
+# /howdy.rb
+require "active_support/all"
+
+my_string = "Hello, world!"
+pp my_string
+
+pp "What's your name?"
+
+their_name = gets
+
+puts "Hello, #{their_name}!"
+
+
+
+
+require "./goodbye.rb"
